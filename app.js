8c59b0ad92e453d67bbe7e02d8da9a24f134c97b
Victor Hugo <victorhugodelciampo@gmail.com>
2021-10-19 12:42:31 -0300

    :construction: feat: add dom's

---
+++ app.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#form');
+const searchInput = document.querySelector('#search');
+const songsContainer = document.querySelector('#songs-container');
+const prevAndNextContainer = document.querySelector('#prev-and-next-container');
+
+const apiURL = 'https://api.lyrics.ovh';
+console.log({ form, searchInput, songsContainer, prevAndNextContainer });
